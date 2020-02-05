df7e07263ad38e59c3e7b006d559c18f244bf4fb
Marco Antonio Martinez Lopez <malomaan@msn.com>
2020-02-04 20:49:59 -0500

    Creación del Proyecto

---
+++ ClinichistoryWEB/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ClinichistoryWEB.Startup))]
+namespace ClinichistoryWEB
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
